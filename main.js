8b3d7f6b329c229f1b5fd90c5954bbc0d9ba99e3
Jonathan Commins <joncom@gmail.com>
2014-05-13 05:53:09 -0700

    First commit.

---
+++ main.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var fs = require('fs');
+
+// Setup paths
+var root = __dirname;
+var impactLibPath = root + '/lib';
+
+// Alter the env to allow impact
+// to run without DOM interaction.
+var Canvas = function() {
+    return {
+        addEventListener: function() { },
+        style: { },
+        getContext: function() {
+            // This is the context
+            return {
+                save: function() { },
+                translate: function() { },
+                rotate: function() { },
+                restore: function() { },
+                drawImage: function() { },
+                strokeRect: function() { },
+                beginPath: function() { },
+                moveTo: function() { },
+                lineTo: function() { },
+                stroke: function() { },
+                clearPath: function() { },
+                scale: function() { },
+                fillRect: function() { }
+            };
+        }
+    };
+};
+global.window = global;
+global.ImpactMixin = {
+    module: function() { return ig; },
+    requires: function() {
+        var requires = Array.prototype.slice.call(arguments);
+        // Go ahead and require the proper files
+        requires.forEach(function(name) {
+            // Ignore any dom ready type stuff on the server.
+            if (name == 'dom.ready') return;
+            var path = name.replace(/\./g, '/');
+            require(impactLibPath + '/' + path);
+        });
+        return ig;
+    },
+    defines: function(func) {
+        func(); // immediately execute
+    },
+    $: function(selector) {
+        return new Canvas();
+    }
+};
+window.document = { };
+window.addEventListener = function() { };
+
+// Canvas should be the only element impact uses on the server.
+window.HTMLElement = Canvas;
+require(impactLibPath + '/impact/impact.js');
+require(impactLibPath + '/game/main.js');
